fcc1bbf13e911ee7107e6d9e7a400bb839d25fd6
phantekzy <mainilotfi@gmail.com>
2026-04-02 17:48:07 +0100

    Sequelize configuration

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,4 +1,13 @@
-import { Sequelize } from "sequelize";
 import "dotenv/config";
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
+  dialect: "postgres",
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+});
+
+export default sequelize;
