a84f8e8481341cefc3a22b8901b254aa0c5ac977
phantekzy <mainilotfi@gmail.com>
2026-04-03 19:35:51 +0100

    name of the product

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -4,4 +4,5 @@ import sequelize from "../config/database";
 export const Product = sequelize.define("product", {
   id: { type: DataTypes.UUID, defaultValue: UUIDV4, primaryKey: true },
   sku: { type: DataTypes.STRING, unique: true },
+  name: { type: DataTypes.STRING, allowNull: false },
 });
