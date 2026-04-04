54a8acbd64fcfcc0a8dfead1c94550a4d077a337
phantekzy <mainilotfi@gmail.com>
2026-04-04 21:58:09 +0100

    auth middleware is done

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -7,5 +7,7 @@ export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
     req.user = decoded;
     next();
-  } catch (error) {}
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
