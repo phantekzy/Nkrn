16bdc227e303076033bde3bf855cd17228671c34
phantekzy <mainilotfi@gmail.com>
2026-04-04 21:57:20 +0100

    verify token

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Response } from "express";
-
+import jwt from "jsonwebtoken";
 export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch (error) {}
 };
