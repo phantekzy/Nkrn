f8ead27770807529a79bfe1875a903a95e039c6c
phantekzy <mainilotfi@gmail.com>
2026-04-04 20:32:22 +0100

    added a token logic verification , and fixed the response to res

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,1 +1,6 @@
-export const authMiddleware = () => {};
+import { NextFunction, Response } from "express";
+
+export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+};
