b5a2275cd2e65675a9be51b236f0684d99f34b8c
phantekzy <mainilotfi@gmail.com>
2026-04-06 19:24:02 +0100

    Select all queries

--- backend/src/services/inventoryService.ts
+++ backend/src/services/inventoryService.ts
@@ -1,11 +1,5 @@
-import { Product } from "../models/Product";
+import { Product } from "../models/Product"
 
-export const fetchAllProducts = async () => {
-  return await Product.findAll();
-};
-
-export const adjustStock = async (id: string, quantity: number) => {
-  return await Product.update({ quantity }, { where: { id } });
-};
-
-// REDO THIS AND UNDERSTAND IT
+export const fetchAllProducts = async() =>{
+    return await Product.findAll();
+}
