bcc932595abaf292e3ff3e1b28f86230082146c6
phantekzy <mainilotfi@gmail.com>
2026-04-07 17:58:45 +0100

    get the list and send it as json

--- backend/src/controllers/inventoryController.ts
+++ backend/src/controllers/inventoryController.ts
@@ -1,3 +1,9 @@
 import { Request, Response } from "express";
+import * as inventoryService from "../services/inventoryService";
 
-export const getInventory = async (req: Request, res: Response) => {};
+export const getInventory = async (req: Request, res: Response) => {
+  try {
+    const products = await inventoryService.fetchAllProducts();
+    res.json(products);
+  } catch (error) {}
+};
