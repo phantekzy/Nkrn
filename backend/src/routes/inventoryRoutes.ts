9e5ead1e989a5bf2039d191afd076b37a4ce2b2e
phantekzy <mainilotfi@gmail.com>
2026-04-07 21:03:36 +0100

    forgot to add the admin middlware here

--- backend/src/routes/inventoryRoutes.ts
+++ backend/src/routes/inventoryRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth";
 import { getInventory, updateStock } from "../controllers/inventoryController";
+import { adminMiddleware } from "../middleware/adminMiddleware";
 
 const router = Router();
 
 router.get("/", authMiddleware, getInventory);
-router.patch("/:id", authMiddleware, updateStock);
+router.patch("/:id", authMiddleware, adminMiddleware, updateStock);
 
 export default router;
