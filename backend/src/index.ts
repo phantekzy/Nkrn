0999cd636c9c3589131bbbc10d50fbe0ad7626e4
phantekzy <mainilotfi@gmail.com>
2026-04-08 07:04:41 +0100

    adjusting the entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import inventoryRoutes from "./routes/inventoryRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("api/inventory", inventoryRoutes);
